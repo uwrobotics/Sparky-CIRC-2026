56fcb7da0eda1510f79b0833751f08808f2029f8
V H <145728917+Vicente-III-H@users.noreply.github.com>
2026-06-25 10:43:29 -0400

    Fix formatting

--- src/led_controller/src/led.cpp
+++ src/led_controller/src/led.cpp
@@ -1,51 +1,55 @@
 #include "led.hpp"
 
 LED::LED(std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>> pub_, rclcpp::Logger logger)
-: pub_(pub_), logger(logger) {};
+: pub_(pub_), logger(logger) {}
 
-void LED::set_colour(float r, float g, float b, float a) {
-    auto colour = std::make_unique<std_msgs::msg::ColorRGBA>();
-    colour->r = r;
-    colour->g = g;
-    colour->b = b;
-    colour->a = a;
+void LED::set_colour(float r, float g, float b, float a)
+{
+  auto colour = std::make_unique<std_msgs::msg::ColorRGBA>();
+  colour->r = r;
+  colour->g = g;
+  colour->b = b;
+  colour->a = a;
 
-    RCLCPP_INFO(logger, "Publishing colour: R(%f), G(%f), B(%f), a(%f)", 
-        colour->r,
-        colour->g,
-        colour->b,
-        colour->a
-    );
-    
-    pub_->publish(std::move(colour));
-};
+  RCLCPP_INFO(
+    logger, "Publishing colour: R(%f), G(%f), B(%f), a(%f)",
+    colour->r,
+    colour->g,
+    colour->b,
+    colour->a
+  );
 
-void LED::set_colour(LEDColours colour_name) {
-    switch(colour_name) {
-        case LEDColours::RED:
-            set_colour(255, 0, 0);
-            break;
-        case LEDColours::ORANGE:
-            set_colour(255, 128, 0);
-            break;
-        case LEDColours::YELLOW:
-            set_colour(255, 255, 0);
-            break;
-        case LEDColours::GREEN:
-            set_colour(0, 255, 0);
-            break;
-        case LEDColours::BLUE:
-            set_colour(0, 0, 255);
-            break;
-        case LEDColours::PURPLE:
-            set_colour(128, 0, 255);
-            break;
-        case LEDColours::WHITE:
-            set_colour(255, 255, 255);
-            break;
-    }
-};
+  pub_->publish(std::move(colour));
+}
 
-void LED::set_random_colour() {
-    set_colour(rand() % 256, rand() % 256, rand() % 256);
-};
+void LED::set_colour(LEDColours colour_name)
+{
+  switch (colour_name) {
+    case LEDColours::RED:
+      set_colour(255, 0, 0);
+      break;
+    case LEDColours::ORANGE:
+      set_colour(255, 128, 0);
+      break;
+    case LEDColours::YELLOW:
+      set_colour(255, 255, 0);
+      break;
+    case LEDColours::GREEN:
+      set_colour(0, 255, 0);
+      break;
+    case LEDColours::BLUE:
+      set_colour(0, 0, 255);
+      break;
+    case LEDColours::PURPLE:
+      set_colour(128, 0, 255);
+      break;
+    case LEDColours::WHITE:
+      set_colour(255, 255, 255);
+      break;
+  }
+}
+
+void LED::set_random_colour()
+{
+  set_colour(rand() % 256, rand() % 256, rand() % 256);
+}
--- src/led_controller/src/led_colour_controller.cpp
+++ src/led_controller/src/led_colour_controller.cpp
@@ -10,86 +10,87 @@ using LifecycleCallback = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterf
 
 class LedColourControllerNode : public rclcpp_lifecycle::LifecycleNode
 {
-    public:
-        explicit LedColourControllerNode(const std::string &name) : rclcpp_lifecycle::LifecycleNode(name) {}
+public:
+  explicit LedColourControllerNode(const std::string & name)
+  : rclcpp_lifecycle::LifecycleNode(name) {}
 
-        LifecycleCallback on_configure(const rclcpp_lifecycle::State &)
-        {
-            RCLCPP_INFO(this->get_logger(), "Configuring LED Colour Controller");
+  LifecycleCallback on_configure(const rclcpp_lifecycle::State &)
+  {
+    RCLCPP_INFO(this->get_logger(), "Configuring LED Colour Controller");
 
-            pub_ = this->create_publisher<std_msgs::msg::ColorRGBA>("led_colour", 10);
+    pub_ = this->create_publisher<std_msgs::msg::ColorRGBA>("led_colour", 10);
 
-            led_ = std::make_shared<LED>(pub_, this->get_logger());
+    led_ = std::make_shared<LED>(pub_, this->get_logger());
 
-            timer_ = this->create_wall_timer(1s, std::bind(&LED::set_random_colour, led_.get()));
-            timer_->cancel(); // Start with the timer stopped
+    timer_ = this->create_wall_timer(1s, std::bind(&LED::set_random_colour, led_.get()));
+    timer_->cancel();         // Start with the timer stopped
 
-            led_->set_colour(LEDColours::YELLOW);
+    led_->set_colour(LEDColours::YELLOW);
 
-            return LifecycleCallback::SUCCESS;
-        }
+    return LifecycleCallback::SUCCESS;
+  }
 
-        LifecycleCallback on_activate(const rclcpp_lifecycle::State& state)
-        {
-            RCLCPP_INFO(this->get_logger(), "Activating LED Colour Controller");
+  LifecycleCallback on_activate(const rclcpp_lifecycle::State & state)
+  {
+    RCLCPP_INFO(this->get_logger(), "Activating LED Colour Controller");
 
-            LifecycleNode::on_activate(state);
+    LifecycleNode::on_activate(state);
 
-            timer_->reset(); // Resume timer on activate
+    timer_->reset();         // Resume timer on activate
 
-            return LifecycleCallback::SUCCESS;
-        }
+    return LifecycleCallback::SUCCESS;
+  }
 
-        LifecycleCallback on_deactivate(const rclcpp_lifecycle::State& state)
-        {
-            RCLCPP_INFO(this->get_logger(), "Deactivating LED Colour Controller");
+  LifecycleCallback on_deactivate(const rclcpp_lifecycle::State & state)
+  {
+    RCLCPP_INFO(this->get_logger(), "Deactivating LED Colour Controller");
 
-            LifecycleNode::on_deactivate(state);
+    LifecycleNode::on_deactivate(state);
 
-            timer_->cancel();
+    timer_->cancel();
 
-            led_->set_colour(LEDColours::RED);
+    led_->set_colour(LEDColours::RED);
 
-            return LifecycleCallback::SUCCESS;
-        }
+    return LifecycleCallback::SUCCESS;
+  }
 
-        LifecycleCallback on_cleanup(const rclcpp_lifecycle::State& state)
-        {
-            RCLCPP_INFO(this->get_logger(), "Cleaning up LED Colour Controller");
+  LifecycleCallback on_cleanup(const rclcpp_lifecycle::State & state)
+  {
+    RCLCPP_INFO(this->get_logger(), "Cleaning up LED Colour Controller");
 
-            timer_.reset();
-            pub_.reset();
+    timer_.reset();
+    pub_.reset();
 
-            return LifecycleCallback::SUCCESS;
-        }
+    return LifecycleCallback::SUCCESS;
+  }
 
-        LifecycleCallback on_shutdown(const rclcpp_lifecycle::State& state)
-        {
-            RCLCPP_INFO(this->get_logger(), "Shuting down LED Colour Controller");
+  LifecycleCallback on_shutdown(const rclcpp_lifecycle::State & state)
+  {
+    RCLCPP_INFO(this->get_logger(), "Shutting down LED Colour Controller");
 
-            timer_.reset();
-            pub_.reset();
+    timer_.reset();
+    pub_.reset();
 
-            return LifecycleCallback::SUCCESS;
-        }
+    return LifecycleCallback::SUCCESS;
+  }
 
-    private:
-        // Using regular publisher to allow colour changing in inactive state
-        std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>> pub_;
-        std::shared_ptr<rclcpp::TimerBase> timer_;
-        std::shared_ptr<LED> led_;
+private:
+  // Using regular publisher to allow colour changing in inactive state
+  std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>> pub_;
+  std::shared_ptr<rclcpp::TimerBase> timer_;
+  std::shared_ptr<LED> led_;
 };
 
 int main(int argc, char * argv[])
 {
-    rclcpp::init(argc, argv);
+  rclcpp::init(argc, argv);
 
-    rclcpp::executors::SingleThreadedExecutor executor;
-    auto node = std::make_shared<LedColourControllerNode>("led_colour_controller_node");
+  rclcpp::executors::SingleThreadedExecutor executor;
+  auto node = std::make_shared<LedColourControllerNode>("led_colour_controller_node");
 
-    executor.add_node(node->get_node_base_interface());
-    executor.spin();
+  executor.add_node(node->get_node_base_interface());
+  executor.spin();
 
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::shutdown();
+  return 0;
 }
