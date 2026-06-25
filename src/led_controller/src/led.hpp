61e43fadf82b4ca610de2c60f005f227e653b1e9
V H <145728917+Vicente-III-H@users.noreply.github.com>
2026-06-25 10:43:29 -0400

    Fix formatting

--- src/led_controller/src/led.hpp
+++ src/led_controller/src/led.hpp
@@ -7,27 +7,29 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/color_rgba.hpp"
 
-enum class LEDColours {
-    RED,
-    ORANGE,
-    YELLOW,
-    GREEN,
-    BLUE,
-    PURPLE,
-    WHITE
+enum class LEDColours
+{
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  PURPLE,
+  WHITE
 };
 
-class LED {
-    public:
-        LED(std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>> pub_, rclcpp::Logger logger);
-        void set_colour(float r, float g, float b, float a = 0);
-        void set_colour(LEDColours colour_name);
-        void set_random_colour();
+class LED
+{
+public:
+  LED(std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>> pub_, rclcpp::Logger logger);
+  void set_colour(float r, float g, float b, float a = 0);
+  void set_colour(LEDColours colour_name);
+  void set_random_colour();
 
-    private:
-        std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>> pub_;
-        rclcpp::Logger logger;
-        std::unique_ptr<std_msgs::msg::ColorRGBA> curr_colour_;
+private:
+  std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>> pub_;
+  rclcpp::Logger logger;
+  std::unique_ptr<std_msgs::msg::ColorRGBA> curr_colour_;
 };
 
 #endif
