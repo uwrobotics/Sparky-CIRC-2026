314874d56e83ae779c4d3d5d5047cecf50673124
Allen Abraham <allenabraham106@gmail.com>
2026-06-29 17:01:36 -0600

    adding ros2_control for ps4 control

--- src/arm_description/launch/display.launch.py
+++ src/arm_description/launch/display.launch.py
@@ -1,30 +1,28 @@
 import os
+import xacro
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    pkg = get_package_share_directory("arm_description")
-    urdf_file = os.path.join(pkg, "urdf", "dummy_urdf.urdf")
+    pkg = get_package_share_directory('arm_description')
+    xacro_file  = os.path.join(pkg, 'urdf', 'arm.control.urdf.xacro')
     rviz_config = os.path.join(pkg, 'config', 'arm_display.rviz')
 
-    with open(urdf_file, "r") as f:
-        robot_description = f.read()
+    robot_description = xacro.process_file(xacro_file).toxml()
 
-    return LaunchDescription(
-        [
-            Node(
-                package="robot_state_publisher",
-                executable="robot_state_publisher",
-                name="robot_state_publisher",
-                parameters=[{"robot_description": robot_description}],
-            ),
-            Node(
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
-                arguments=['-d', rviz_config]
-            ),
-        ]
-    )
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            arguments=['-d', rviz_config],
+        ),
+    ])
