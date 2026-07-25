4354111b51c765c717e8dc88d7a915b43293c6e8
V H <145728917+Vicente-III-H@users.noreply.github.com>
2026-07-24 22:36:34 -0400

    Upload code for arduino communication

---
+++ arduino/arduino_logic.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  Serial.println("\nI2C Scanner");
+}
+
+void loop() {
+  byte error, address;
+  int nDevices = 0;
+
+  Serial.println("Scanning...");
+
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) Serial.print("0");
+      Serial.println(address, HEX);
+      nDevices++;
+    }
+  }
+
+  if (nDevices == 0) Serial.println("No I2C devices found");
+  else Serial.println("done\n");
+
+  delay(5000); // Scan every 5 seconds
+}
